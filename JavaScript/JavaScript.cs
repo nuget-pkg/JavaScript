3106d83b7623cca601658eccf220db2b89707f8d
JavaCommons Technologies <javacommons@gmail.com>
2026-02-10 03:07:03 +0900

    .

--- JavaScript/JavaScript.cs
+++ JavaScript/JavaScript.cs
@@ -4,51 +4,50 @@
     using System.Reflection;
     public class JavaScript //: Global.IEasyScript
     {
-        private readonly dynamic? _engine;
-        static readonly Assembly? assembly = null;
-        public static readonly Type myType;
-        //public static readonly dynamic? instance = null;
+        private static readonly Assembly? assembly = null;
+        private static readonly Type engineType;
+        private readonly dynamic? engine;
         static JavaScript()
         {
             var thisAssembly = typeof(JavaScript).Assembly;
             assembly = Sys.LoadFromResource(thisAssembly, "Javascript:JavaScriptInternals.dll");
-            myType = assembly!.GetType("Global.JavaScriptInternals")!;
+            engineType = assembly!.GetType("Global.JavaScriptInternals")!;
         }
         public JavaScript(params Assembly[] assemblies)
         {
-            _engine = Activator.CreateInstance(myType!, [assemblies])!;
+            engine = Activator.CreateInstance(engineType!, [assemblies])!;
         }
         public dynamic? Call(string name, params object[] vars)
         {
-            return _engine!.Call(name, vars);
+            return engine!.Call(name, vars);
         }
         public dynamic? Evaluate(string script, params object[] vars)
         {
-            return _engine!.Evaluate(script, vars);
+            return engine!.Evaluate(script, vars);
         }
         public dynamic? EvaluateFile(string fileName, string script, params object[] vars)
         {
-            return _engine!.EvaluateFile(fileName, script, vars);
+            return engine!.EvaluateFile(fileName, script, vars);
         }
 
         public void Execute(string script, params object[] vars)
         {
-            _engine!.Execute(script, vars);
+            engine!.Execute(script, vars);
         }
 
         public void ExecuteFile(string fileName, string script, params object[] vars)
         {
-            _engine!.ExecuteFile(fileName, script, vars);
+            engine!.ExecuteFile(fileName, script, vars);
         }
 
         public dynamic? GetValue(string name)
         {
-            return _engine!.GetValue(name);
+            return engine!.GetValue(name);
         }
 
         public void SetValue(string name, dynamic? value)
         {
-            _engine!.SetValue(name, value);
+            engine!.SetValue(name, value);
         }
     }
 }
